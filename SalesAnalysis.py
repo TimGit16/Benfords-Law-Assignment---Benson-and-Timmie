0275eef535434def9800de40a12e310c3c335679
BensonLiu06 <129234480+BensonLiu06@users.noreply.github.com>
2023-04-20 19:51:05 -0400

    Added comments

--- SalesAnalysis.py
+++ SalesAnalysis.py
@@ -7,21 +7,21 @@ folder = os.getcwd()
 def load_sales_data():
     while True:
         csvName = "sales.csv"
-        fileinput = input("Please enter your file name (eg. sales.csv): ")
+        fileinput = input("Please enter your file name (eg. sales.csv): ") #allows the user to type in the file of the name (sales.csv)
         if os.path.isfile(folder+"\\"+fileinput):
-            if fileinput != csvName:
+            if fileinput != csvName: #if the file name is not sales.csv then it will return invalid until the user types in a valid input
                 print("Invalid File Name")
-            elif fileinput == csvName:
+            elif fileinput == csvName: 
                 print("Valid File")
                 # fileName = folder + "\\" + csvName
-                with open(fileinput) as file:
+                with open(fileinput) as file: 
                     salesData = file.read() # Assigns all the text from sales.csv to variable as a string
                     salesData = salesData.splitlines() # Splits lines marked by \n to create a list wihtout \n
                     salesData.pop(0) # Removes "Postal Code, Sales" line
                     for i in range(len(salesData)): # Iterates through salesList to make each item be only the number after the comma
                         salesData[i] = salesData[i].split(",")[1] # Replaces item at index i to be the second item after splitting with delimiter ,
                 return salesData
-        else:
+        else: 
             print("File Does Not Exist In Current Working Directory")
 
 def digCount(num):
@@ -52,16 +52,16 @@ def reportFreq(digit):
     print("The first digit frequency for",digit,"is:",str(digFreqCalc(digCount(digit)))+"%") # prints digit frequency of parameter value digit
 
 def graph():
-    x_numbers = []
-    y_numbers = []
-    for n in range(1,10):
-        x_numbers.append(str(n))
-        y_numbers.append(digFreqCalc(digCount(n)))
+    x_numbers = [] #appends the first digits to an empty list
+    y_numbers = [] #appends the frequency to an empty list 
+    for n in range(1,10): 
+        x_numbers.append(str(n)) #appends the first digits
+        y_numbers.append(digFreqCalc(digCount(n))) #appends the frequency
 
-    fig, ax = plt.subplots()
-    bar_container = ax.bar(x_numbers, y_numbers)
-    ax.set(xlabel = "Digit", ylabel='Frequency (%)', title="Benford's Law First Digit Frequency")
-    ax.bar_label(bar_container)
+    fig, ax = plt.subplots() #used to plot subplots which is the layout of the graph
+    bar_container = ax.bar(x_numbers, y_numbers) # numbers for the x and y axis
+    ax.set(xlabel = "Digit", ylabel='Frequency (%)', title="Benford's Law First Digit Frequency") #adds a title and labels to the x and y axis
+    ax.bar_label(bar_container) #labels the x and y axis with the first digit and the frequencies
 
     plt.show()
 
@@ -88,7 +88,7 @@ def createfile():
         resultFile.write(xList[item] + "," + str(yList[item])+"\n")
     resultFile.close()
 
-def printMenu():
+def printMenu(): #displays the main menu
     print ('''
             Sales Fraud Check \n 
             1. Load Data \n
@@ -111,10 +111,10 @@ while userInput != exitCondition:
     printMenu()
     userInput = input()
 
-    if userInput == loadDataOption:
-        salesList = load_sales_data()
+    if userInput == loadDataOption: #loads the sale's data if user enters "1"
+        salesList = load_sales_data() 
 
-    elif userInput == BLCOption:
+    elif userInput == BLCOption: # Does Benford's Law compliance check 
         for num in range (1,10):
             reportFreq(num)
         if 29 <= digFreqCalc(digCount(1)) <= 32:
@@ -122,10 +122,10 @@ while userInput != exitCondition:
         else:
             print("This data set is likely to be fraudulent")
 
-    elif userInput == showGraphOption:
+    elif userInput == showGraphOption: #shows an image of the graph to the user
         graph()
 
-    elif userInput == exportDataOption:
+    elif userInput == exportDataOption: #exports the data into a file for the user
         createfile()
         
     elif userInput != exitCondition: 
